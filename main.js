eaf42177daa65d902a60826c6d4aa445f69dc122
vicgoodall <vic.goodall@icloud.com>
2022-12-21 18:06:21 +0000

    starting to build the quiz section

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
     
-
-
-function answerQuiz() {
     $("#sunshine").click(function() {
-        $(".question-box > p").fadeOut().text("Coastal or city?");
-        $(this).text("Coastal");
+        $(".question-box > p").text("Seaside or city?");
+        $(this).text("Seaside");
         $("#snow").text("City");
+        $(".btn1").attr("id", "seaside");
+        $(".btn2").attr("id", "suncity");
+
+    })
+
+    $("#snow").click(function() {
+        $(".question-box > p").text("Mountains or city?");
+        $(this).text("Mountains");
+        $("#sunshine").text("City");
+        $(".btn1").attr("id", "snowcity");
+        $(".btn2").attr("id", "mountains");
+
+    })
+
+    $("#seaside").click(function() {
+        $(".question-box > p").text("Adventuring or partying?");
+        $(this).text("Adventuring");
+        $("#suncity").text("Partying");
     })
 }
 
-});
+);
