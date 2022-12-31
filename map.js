9715e6d39af109044ef2e7f26233ebd371de8b17
vicgoodall <vic.goodall@icloud.com>
2022-12-31 20:52:26 +0000

    amended map

---
+++ map.js
@@ -0,0 +1,17 @@
+    // Initialize and add the map
+    function initMap() {
+        // The location of Uluru
+        const swansea = { lat: 51.6214, lng: 3.9436 };
+        // The map
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 7,
+          center: swansea,
+        });
+        //office marker
+        const marker = new google.maps.Marker({
+          position: swansea,
+          map: map,
+        });
+      };
+      
+      window.initMap = initMap;
